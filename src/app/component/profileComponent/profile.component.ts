6d18a5a86b581e2a9f5acf6431bf61f5f7e3e0b3
Entangled-Logic Alpha <entangledlogic@outlook.com>
2025-09-01 01:49:42 -0700

    image api

--- src/app/component/profileComponent/profile.component.ts
+++ src/app/component/profileComponent/profile.component.ts
@@ -35,7 +35,7 @@ export class ProfileComponent implements OnInit {
 
         if (importForm.get('Avatar')?.value !== '') {
 
-          this.avatarURL = `${environment.IMAGE_REPO}/${importForm.get('Avatar')?.value}`;
+          this.avatarURL = ``${environment.SERVER_URI}/images/${importForm.get('Avatar')?.value}`;
 
         }
 
