39a91cc89d113f884de07d6ee40d2c98680ce1c5
Entangled-Logic Alpha <entangledlogic@outlook.com>
2025-09-21 03:31:42 -0700

    Update header.component.ts

--- src/app/component/headerComponent/header.component.ts
+++ src/app/component/headerComponent/header.component.ts
@@ -68,7 +68,7 @@ export class HeaderComponent implements OnInit {
 
   onNavigate003(): void {
 
-    window.location.href = 'https://houseofredcamera.webp.com';
+    window.location.href = 'https://your-url.com'; //sub
 
     this.isHome = false;
 
